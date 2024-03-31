91d38408b9d200fe37a180dbad4bae35ee48bf84
Fiddar Putri <pfiddar@gmail.com>
2024-03-31 11:43:46 +0700

    add product object

--- newproduct.php
+++ newproduct.php
@@ -1,3 +1,57 @@
+<!-- product variable -->
+<?php
+    $product = [[
+        "id" => 1,
+        "nama" => "Chessecake",
+        "harga" => "Rp. 20.000",
+        "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing"
+    ],
+    [
+        "id" => 2, 
+        "nama" => "Choco Muffin",
+        "img" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj66Y47aMrlY3zLZYnOPnK-gFp7q4ucupD-w&usqp=CAU",
+        "harga" => "Rp. 10.000",
+        "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing"
+    ],
+    [
+        "id" => 3, 
+        "nama" => "Chiffon Cake",
+        "img" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGtFHiPO61y9cO5KEzI5ksSborCuCEe-7R5Q&usqp=CAU",
+        "harga" => "Rp. 25.000",
+        "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing"
+    ],
+    [
+        "id" => 4, 
+        "nama" => "Croissant",
+        "img" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq86INfgcDFfiaMYw8768L-3eEBhpXV0sGxQ&usqp=CAU",
+        "harga" => "Rp. 20.000",
+        "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing"
+    ],
+    [
+        "id" => 5, 
+        "nama" => "Strawberry Chessecake",
+        "img" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIAG4LfhcfWMXcOeVzq6Yhe-jrg3jy-_Y6Ow&usqp=CAU",
+        "harga" => "Rp. 45.000",
+        "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing"
+    ],
+    [
+        "id" => 6, 
+        "nama" => "Nutella Stuffed Cookies",
+        "img" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUAIG8yEoS1BmkZ0crgEsAAvcvapl9aSjf_A&usqp=CAU",
+        "harga" => "Rp. 15.000",
+        "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing"
+    ],
+    [
+        "id" => 7, 
+        "nama" => "Biscoff Stuffed Cookies",
+        "img" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnBHEMDvWEtoRoO8Y624aSyK2fygrS9k6iSw&usqp=CAU",
+        "harga" => "Rp. 15.000",
+        "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing"
+    ],
+];
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -103,6 +157,7 @@
             <!-- main content -->
             <div class="content">
                 <div class="row"  id="content">
+                    
                 </div>
             </div>
         </div>
